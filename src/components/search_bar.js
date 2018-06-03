f9b0c0e8a90a6cba9607f9a1686f357a8c15bf7d
Daniel Anatolie <daniel.anatolie@gmail.com>
2018-06-03 09:37:34 -0700

    Updated video components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {searchTerm: 'Starting Value'};
+        this.state = {searchTerm: ''};
     }
 
     render() {
