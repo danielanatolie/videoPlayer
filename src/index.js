a086f6e8e881ad1f4d2d68f741484b41e1e1d0a3
Daniel Anatolie <daniel.anatolie@gmail.com>
2018-06-03 08:30:00 -0700

    1. Added search bar 2. Added YouTube API, video data stored in App state upon entry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyDrlK8wFrLk8d5t6ZzKNTlMUkIdpDEimcU';
 
-const App = () => {
-    return <div>Hello World!</div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'blockchain'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
